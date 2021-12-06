ed73ba1ce6defa9e02149a7f82b205304621b084
Catalin Teodorescu <dustfeather@gmail.com>
2021-12-06 18:38:31 +0200

    weekly

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ $(window).on('load', function () {
         .then(response => response.json())
         .then(data => renderChart(data));
 
+    /**
+     * @param data
+     */
     function renderChart(data) {
         data = parseData(data);
         const chart = LightweightCharts.createChart(document.body, {
@@ -29,17 +32,14 @@ $(window).on('load', function () {
                 borderColor: '#485c7b',
             }
         });
-        const lineSeries = chart.addLineSeries();
-        lineSeries.setData(data.lineData);
-        const candleSeries = chart.addCandlestickSeries({
-            upColor: '#4bffb5',
-            downColor: '#ff4976',
-            borderDownColor: '#ff4976',
-            borderUpColor: '#4bffb5',
-            wickDownColor: '#ff4976',
-            wickUpColor: '#4bffb5',
+        const close = chart.addLineSeries({
+            color: 'orange'
         });
-        candleSeries.setData(data.candleData);
+        close.setData(data.closeData);
+        const predicted = chart.addLineSeries({
+            color: 'aqua'
+        });
+        predicted.setData(data.predictedData);
     }
 
     /**
@@ -47,22 +47,19 @@ $(window).on('load', function () {
      * @returns {*[]}
      */
     function parseData(data) {
-        let result = {'lineData': [], 'candleData': []};
+        let result = {'predictedData': [], 'closeData': []};
         if (data.data) {
-            let line = data.data[0];
-            let candle = data.data[1];
-            $(line.x).each(function (i, e) {
+            let predicted = data.data[0];
+            let close = data.data[1];
+            $(predicted.x).each(function (i, e) {
                 let time = e.replace('T00:00:00', '');
-                result.lineData.push({
+                result.predictedData.push({
                     time: time,
-                    value: line.y[i]
+                    value: predicted.y[i]
                 });
-                result.candleData.push({
+                result.closeData.push({
                     time: time,
-                    open: candle.open[i],
-                    close: candle.close[i],
-                    high: candle.high[i],
-                    low: candle.low[i],
+                    value: close.y[i]
                 });
             });
 
